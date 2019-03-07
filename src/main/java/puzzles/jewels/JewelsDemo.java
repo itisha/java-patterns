5ee546a5a718ff556f9054e9bbcfb74465680a32
Tsikhon Kuprevich <T.Kuprevich@mps-solutions.de>
2019-03-07 12:28:14 +0300

    Initialization sequence

---
+++ src/main/java/puzzles/jewels/JewelsDemo.java
@@ -0,0 +1,28 @@
+package puzzles.jewels;
+
+import org.junit.Assert;
+
+import java.util.stream.Stream;
+
+public class JewelsDemo {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        String j = "aA";
+        String s = "aAAbbbb";
+        Assert.assertEquals(3, solution.numJewelsInStones(j, s));
+    }
+
+
+}
+
+
+class Solution {
+    public int numJewelsInStones(String j, String s) {
+        if (j == null || s == null)
+            return 0;
+
+        return (int) Stream.of(s.split("")).filter(j::contains).count();
+    }
+}
