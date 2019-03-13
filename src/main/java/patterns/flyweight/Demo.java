48a357b681f8807704cf55262d076558727f781e
Tsikhon Kuprevich <T.Kuprevich@mps-solutions.de>
2019-03-13 16:50:10 +0300

    Flyweight names

---
+++ src/main/java/patterns/flyweight/Demo.java
@@ -0,0 +1,16 @@
+package patterns.flyweight;
+
+public class Demo {
+    public static void main(String[] args) {
+        // User user = new User("John Smith");
+        // User user1 = new User("Jane Smith");
+
+        // have "Smith" in common
+
+        User2 user2 = new User2("John Smith");
+        User2 user3 = new User2("Jane Smith");
+
+        System.out.println(user2.getFullName());
+        System.out.println(user3.getFullName());
+    }
+}
---
+++ src/main/java/patterns/flyweight/User.java
@@ -0,0 +1,9 @@
+package patterns.flyweight;
+
+class User {
+    private String fullName;
+
+    public User(String fullName) {
+        this.fullName = fullName;
+    }
+}
