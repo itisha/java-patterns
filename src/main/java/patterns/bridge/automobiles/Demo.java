e1adf2a4bead0580a0d29689261711a57831e80f
Tsikhon Kuprevich <T.Kuprevich@mps-solutions.de>
2019-04-19 16:03:43 +0300

    bridge pattern

--- src/main/java/patterns/bridge/automobiles/Demo.java
+++ src/main/java/patterns/bridge/automobiles/Demo.java
@@ -9,6 +9,8 @@ public class Demo {
     public static void main(String[] args) {
 
         //Decouple abstraction Car from implementation Make
+        //Easy to add new classes
+        //Ho huge and complicated hierarchy
 
         //now we can create any type of car from any make:
         Car car = new Sedan(new Honda());
